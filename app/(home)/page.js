6cc64b5385ee1e05f799c13076ffd590ae5e3202
mohammad-moa <aahrari5@gmail.com>
2023-06-09 17:21:37 +0330

    updated left panel ui (test info form)

--- app/(home)/page.js
+++ app/(home)/page.js
@@ -3,14 +3,15 @@
 import LeftPanel from "@/components/LeftPanel/LeftPanel";
 import RightPanel from "@/components/RightPanel/RightPanel";
 import { Grid } from "@mui/material";
+import { deepPurple } from "@mui/material/colors";
 
 export default function Home() {
   return (
     <Grid container height={"100vh"}>
-      <Grid item lg={3}>
+      <Grid item lg={3} bgcolor={"#ede7f6b3"} padding={"45px 30px"}>
         <LeftPanel />
       </Grid>
-      <Grid item lg={9}>
+      <Grid item lg={9} padding={"45px 30px"}>
         <RightPanel />
       </Grid>
     </Grid>
--- components/LeftPanel/LeftPanel.jsx
+++ components/LeftPanel/LeftPanel.jsx
@@ -1,7 +1,58 @@
+import { Stack, Typography } from "@mui/material";
+import { deepPurple } from "@mui/material/colors";
+
 export default function LeftPanel() {
+
+    const subtitleColor = deepPurple["300"]
+    const typographyStyle = {
+        fontSize: "18px",
+        fontWeight: 500,
+        paddingTop: "5px"
+    }
+
     return (
-        <span>
-            Left
-        </span>
+        <Stack
+            direction={"column"}
+        >
+            <Typography variant="h4" marginBottom={3}>
+                Mohammad Ahrari
+            </Typography>
+
+            <div style={{ marginTop: "20px" }}>
+                <Typography variant="subtitle1" color={subtitleColor}>
+                    Email
+                </Typography>
+                <Typography sx={typographyStyle}>
+                    aahrari5@gmail.com
+                </Typography>
+            </div>
+
+            <div style={{ marginTop: "20px" }}>
+                <Typography variant="subtitle1" color={subtitleColor}>
+                    Phone
+                </Typography>
+                <Typography sx={typographyStyle}>
+                    09170000000
+                </Typography>
+            </div>
+
+            <div style={{ marginTop: "20px" }}>
+                <Typography variant="subtitle1" color={subtitleColor}>
+                    Address
+                </Typography>
+                <Typography sx={typographyStyle}>
+                    Iran - Fars - Lar
+                </Typography>
+            </div>
+
+            <div style={{ marginTop: "20px" }}>
+                <Typography variant="subtitle1" color={subtitleColor}>
+                    Message
+                </Typography>
+                <Typography sx={typographyStyle}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus nihil hic aliquid, praesentium magnam ...
+                </Typography>
+            </div>
+        </Stack>
     )
 }
